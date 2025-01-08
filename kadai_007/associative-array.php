f8dd67345f65f7549b99319ce2208ebaa08eae46
mikeru-design-git <design.mikeru@gmail.com>
2025-01-08 23:18:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai07</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      // 連想配列
+      $products_data = [
+        'name' => 'onion',
+        'price' => 200,
+        'weight' => 160,];
+      // 出力
+      print_r($products_data);
+    ?>
+  </p>
+</body>
+</html>
